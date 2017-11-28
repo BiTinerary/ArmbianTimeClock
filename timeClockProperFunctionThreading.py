01946c4c80a1a9e12e477c709e1bda2ae947b667
Izaac <izaac.kirscht@gmail.com>
2017-11-27 19:44:00 -0600

    Update timeClockProperFunctionThreading.py

--- timeClockProperFunctionThreading.py
+++ timeClockProperFunctionThreading.py
@@ -43,7 +43,7 @@ def timeCard(action, employee, data):
                 employeeLogFile.write(timeCardData)
         employeeLogFile.close()
 
-        threadFunction(MasterLog, args=[employee, timeCardData])
+        threadFunction(MasterLog, [employee, timeCardData])
         print "%s %s on %s at %s" % (employee, action, data[0], data[1])
 
 def threadFunction(func, array):
