77e977889dd7c82b6a89199547f17ff0ee4146fb
Izaac <izaac.kirscht@gmail.com>
2017-11-27 22:06:33 -0600

    Update timeClockProperFunctionThreading.py

--- timeClockProperFunctionThreading.py
+++ timeClockProperFunctionThreading.py
@@ -2,6 +2,8 @@ import OPi.GPIO as GPIO
 import threading, datetime, time, sys, os
 
 employeeName = sys.argv[1]
+redLedPin = 7
+greenLedPin = 15
 dtNow = datetime.datetime.now()
 punchDayTime = [dtNow.strftime("%m-%d-%Y"), dtNow.strftime("%H:%M")]
 
@@ -27,21 +29,27 @@ def blink(color, times):
                         GPIO.output(color, GPIO.LOW)
                         GPIO.cleanup()
 
-def MasterLog(employee, data):
+def MasterLog(employee, data): ## GOOGLE API CREDZ PARAM
         with open('MasterTimeClockLog.csv', 'a+') as MasterLog:
                 MasterLog.write("%s, %s\n" % (employee, data))
         MasterLog.close()
         print 'Master Log Written Succesfully!'
-
+        
+        ## PARSE MASTERLOG FOR HUMAN VIEWING
+        ## CREATE MASTER SHEET
+        ## GOOGLE CREDZ API, PUNCH GSHEET WITH MASTER SHEET 
+        
         blink(['red', 'green'], 3)
 
-def timeCard(action, employee, data):
+def timeCard(punch):
+        action, employee, data = punch[0], punch[1], punch[2]
+        
         with open("%s.csv" % (employee), 'a+') as employeeLogFile:
                 timeCardData = "%s, %s, %s\n" % (action, data[0], data[1])
                 employeeLogFile.write(timeCardData)
         employeeLogFile.close()
 
-        MasterLogThread = threadFunction(MasterLog, [employee, timeCardData])
+        MasterLogThread = threadFunction(MasterLog, [employee, timeCardData]) ## GOOGLE API CREDZ PARAM
         MasterLogThread.start()
         print "%s %s on %s at %s" % (employee, action, data[0], data[1])
 
@@ -49,23 +57,20 @@ def threadFunction(func, array):
         return threading.Thread(target=func, args=(array))
 
 def everySwipe():
-        redLedPin = 7
-        greenLedPin = 15
-
+        
         try:
                 if dtNow.time() < datetime.time(12):
-                        action = ["CLOCKIN", employeeName, punchDayTime]
-                        actionThread = threadFunction(timeCard, action).start()
+                        punch = ["CLOCKIN", employeeName, punchDayTime]
+                        #actionThread = threadFunction(timeCard, action).start()
                         ledThread = threadFunction(blink, [greenLedPin, 3])
 
                 elif dtNow.time() > datetime.time(12):
-                        action = ["CLOCKOUT", employeeName, punchDayTime]
-                        actionThread = threadFunction(timeCard, action).start
+                        punch = ["CLOCKOUT", employeeName, punchDayTime]            
+                        #actionThread = threadFunction(timeCard, action).start
                         ledThread = threadFunction(blink, [greenLedPin, 6])
 
-                actionThread.start()
+                timeCard(punch)
                 ledThread.start()
-                actionThread.join()
-                ledThread.join()
         except:
                 blink(redLedPin, 9999)
+everySwipe()  
