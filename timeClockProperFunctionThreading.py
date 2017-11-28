8ded52ce19a6d400e1f3e716d0dabcf86df8c92b
Izaac <izaac.kirscht@gmail.com>
2017-11-27 19:26:07 -0600

    Update timeClockProperFunctionThreading.py

--- timeClockProperFunctionThreading.py
+++ timeClockProperFunctionThreading.py
@@ -5,11 +5,11 @@ employeeName = sys.argv[1]
 dtNow = datetime.datetime.now()
 punchDayTime = [dtNow.strftime("%m-%d-%Y"), dtNow.strftime("%H:%M")]
 
-redLedPin = 22
-greenLedPin = 18
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(redLedPin, GPIO.OUT)
-#GPIO.setup(greenLedPin, GPIO.OUT)
+redLedPin = 12
+greenLedPin = 16
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(redLedPin, GPIO.OUT)
+GPIO.setup(greenLedPin, GPIO.OUT)
 
 def blink(color, times):
         def onOff(color):
@@ -22,8 +22,12 @@ def blink(color, times):
                 if len(color) == 2:
                         for redGreen in color:
                                 onOff(redGreen)
-                elif len(color) == 1:
-                        onOff(color)
+                else:
+                        GPIO.cleanup()
+                        time.sleep(.2)
+                        GPIO.output(greenLedPin, GPIO.HIGH)
+                        time.sleep(.2)
+                        GPIO.output(greenLedPin, GPIO.LOW)
 
 def MasterLog(employee, data):
         with open('MasterTimeClockLog.csv', 'a+') as MasterLog:
@@ -40,31 +44,25 @@ def timeCard(action, employee, data):
         employeeLogFile.close()
 
         threadFunction(MasterLog, args=[emplyee, timeCardData])
-        #threading.Thread(target=MasterLog, args=(employee, timeCardData)).start()
-        #MasterLog(employee, timeCardData)
         print "%s %s on %s at %s" % (employee, action, data[0], data[1])
 
 def threadFunction(func, array):
         return threading.Thread(target=func, args=(array))
 
-while True:
+#while True:
 #try:
-        if dtNow.time() < datetime.time(12):
-                action = ["CLOCKIN", employeeName, punchDayTime]
-                actionThread = threadFunction(timeCard, punchIn)
-                ledThread = threadFunction(blink, [greenLedPin, 3])
-        #timeCard("CLOCKIN", employeeName, punchDayTime)
-        #threading.Thread(target=timeCard).start()
+    if dtNow.time() < datetime.time(12):
+            action = ["CLOCKIN", employeeName, punchDayTime]
+            actionThread = threadFunction(timeCard, punchIn)
+            ledThread = threadFunction(blink, [greenLedPin, 3])
 
-        elif dtNow.time() > datetime.time(12):
-                action = ["CLOCKOUT", employeeName, punchDayTime]
-                threadFunction(timeCard, punchOut)
-                threadFunction(blink, [greenLedPin, 7])
+    elif dtNow.time() > datetime.time(12):
+            action = ["CLOCKOUT", employeeName, punchDayTime]
+            threadFunction(timeCard, punchOut)
+            threadFunction(blink, [greenLedPin, 7])
 
         action.start()
         ledThread.start()
         actionThread.join()
         ledThread.join()
-        #threading.Thread(target=clockOutThread).start()
-        #threading.Thread(target=blink, args=(greenLedPin, 7)).start()
 #except:
