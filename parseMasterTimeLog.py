b0ea0e3aedd1ef440737005c0f787c9afb18db3f
BiTinerary <Zack@bitinerary.com>
2017-11-30 11:21:50 -0600

    rm excess and BU

--- parseMasterTimeLog.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open('masterTimeLog.csv', 'r') as parseMasterLog:
-	reader = csv.reader(parseMasterLog)
-	swipes = list(reader)
-
-	for each in swipes:
-		if each[1] == 'CLOCKIN':
-			print "%s %s @ %s on %s" % (each[0], each[1], each[3], each[2])
